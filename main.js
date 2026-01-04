112e905c2e270562bc21c04ee539d24ffd9344a9
Chandrahas10 <chandrahaskori9@gmail.com>
2026-01-04 19:14:02 +0530

    Add website files

---
+++ main.js
@@ -0,0 +1,9 @@
+
+  const hamburger = document.getElementById("hamburger");
+  const nav = document.getElementById("nav");
+
+  hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    nav.classList.toggle("active");
+  });
+
